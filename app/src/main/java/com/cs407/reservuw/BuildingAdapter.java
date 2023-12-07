3d4f5f03180c3ab820c20d09852d93d7ccc957ce
sunkyojayjung <sunkyo98@hotmail.com>
2023-12-07 08:38:34 -0600

    buildingView file UPDATED (implemented structure of RecyclerView)

---
+++ app/src/main/java/com/cs407/reservuw/BuildingAdapter.java
@@ -0,0 +1,37 @@
+package com.cs407.reservuw;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import java.util.List;
+
+public class BuildingAdapter extends RecyclerView.Adapter<BuildingViewHolder> {
+
+    Context context;
+    List<item> items;
+
+    public BuildingAdapter(Context context, List<item> items) {
+        this.context = context;
+        this.items = items;
+    }
+
+    @NonNull
+    @Override
+    public BuildingViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        return new BuildingViewHolder(LayoutInflater.from(context).inflate(R.layout.item_view,parent,false));
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull BuildingViewHolder holder, int position) {
+        holder.roomNumView.setText(items.get(position).getRoomNum());
+    }
+
+    @Override
+    public int getItemCount() {
+        return items.size();
+    }
+}
