49a642646b038a02f5cee918822dc68122de2fe2
sunkyojayjung <sunkyo98@hotmail.com>
2023-12-07 11:18:39 -0600

    Structure of Room View ADDED

---
+++ app/src/main/java/com/cs407/reservuw/roomView.java
@@ -0,0 +1,14 @@
+package com.cs407.reservuw;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class roomView extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_room_view);
+    }
+}
