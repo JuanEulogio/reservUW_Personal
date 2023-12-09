73dd50cb1b3bed53e61a85185a1c710be5169721
juaneulogio <jceulogio243@gmail.com>
2023-12-09 12:48:54 -0600

    urgent map update:
    - changes= the map is now in the main menu activity and its not a fragment. Also we changed from binding to setOnNavigationItemSelectedListener method
    -user location shown
    - user location custom marker set

--- app/src/main/java/com/cs407/reservuw/MainMenu.java
+++ app/src/main/java/com/cs407/reservuw/MainMenu.java
@@ -1,11 +1,33 @@
 package com.cs407.reservuw;
 
+import static android.content.ContentValues.TAG;
+
+import static com.google.android.material.internal.ContextUtils.getActivity;
+
 import android.annotation.SuppressLint;
+import android.content.Context;
+import android.content.pm.PackageManager;
+import android.graphics.Bitmap;
+import android.graphics.Canvas;
+import android.graphics.drawable.Drawable;
+import android.location.Location;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.MenuItem;
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import com.cs407.reservuw.databinding.ActivityMainMenuBinding;
+import com.cs407.reservuw.roomDB.uwRoomDatabase;
+import com.google.android.gms.location.FusedLocationProviderClient;
+import com.google.android.gms.location.LocationCallback;
+import com.google.android.gms.location.LocationServices;
+import com.google.android.gms.maps.MapFragment;
+import com.google.android.gms.maps.model.BitmapDescriptor;
+import com.google.android.gms.maps.model.BitmapDescriptorFactory;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.android.libraries.places.api.Places;
+import com.google.android.libraries.places.api.net.PlacesClient;
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 import java.io.*;
 import androidx.annotation.NonNull;
@@ -16,55 +38,232 @@ import android.view.MenuItem;
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 import com.google.android.material.navigation.NavigationBarView;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
+import androidx.databinding.DataBindingUtil;
+import androidx.databinding.ViewDataBinding;
 import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentManager;
 import androidx.fragment.app.FragmentTransaction;
 import android.os.Bundle;
+import com.google.android.gms.maps.SupportMapFragment;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.room.Room;
 
-public class MainMenu extends AppCompatActivity {
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
 
 
+public class MainMenu extends AppCompatActivity implements OnMapReadyCallback {
+
+
+
+    private static final int PERMISSIONS_REQUEST_ACCESS_FINE_LOCATION = 1; // could've been any number!
+
+    private FusedLocationProviderClient fusedLocationClient;
+    private LocationCallback locationCallback;
+
+    private GoogleMap mMap;
+
+    private Location lastKnownLocation;
+
+
+    // The entry point to the Places API.
+    private PlacesClient placesClient;
+
+    //TODO: daatabase
+    //uwRoomDatabase db = Room.databaseBuilder(this, uwRoomDatabase.class, "uwRoomDatabase").build();
+
+    //grant permission
+    private boolean locationPermissionGranted;
+
+    private final LatLng uwMadisonLocation = new LatLng(43.075404393142115, -89.40341145630344);
 
     //TODO: why is this syntax one right and not "MainActivityBinding"?
-    ActivityMainMenuBinding binding;
+    //ActivityMainMenuBinding binding;
 
     //TODO: Take notes why binding is prefered bottomNavBAr implementation method
     @Override
     protected void onCreate(Bundle savedInstanceState){
         Intent intent= getIntent();
-
-
         super.onCreate(savedInstanceState);
-        binding = ActivityMainMenuBinding.inflate(getLayoutInflater());
-        setContentView(binding.getRoot());
+        setContentView(R.layout.activity_main_menu);
 
-        binding.bottomNavigationView.setBackground(null);
-        binding.bottomNavigationView.setOnItemSelectedListener(item -> {
-            int itemId = item.getItemId();
-            if(itemId== R.id.favorites){
-                //go to favorites pg
-                return true;
-            } else if (itemId== R.id.reserves) {
-                //go to reserve list
-                return true;
-            }else if(itemId== R.id.logout){
-                //go to logout and sign out user
-                goToLogin();
-                return true;
+        // Construct a PlacesClient
+        Places.initialize(getApplicationContext(), "AIzaSyA16zHnssTmYHbA2HheEq1zwg8ZHILUp1A");
+        placesClient = Places.createClient(this);
+
+        // Obtain the SupportMapFragment and get notified when the map is ready to be used.
+        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
+                .findFragmentById(R.id.map);
+        mapFragment.getMapAsync(this);
+
+        //binding = new ActivityMainMenuBinding(findViewById(R.id.bottomNavigationView));
+        BottomNavigationView binding = findViewById(R.id.bottomNavigationView);
+        binding.setSelectedItemId(R.id.logout);
+        binding.setSelectedItemId(R.id.reserves);
+        binding.setSelectedItemId(R.id.favorites);
+
+        // Perform item selected listener
+        binding.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+
+                if(item.getItemId() == R.id.favorites) {
+
+                }else if(item.getItemId() == R.id.reserves){
+
+                }else if(item.getItemId() == R.id.logout){
+                    goToLogin();
+
+                    //TODO: optional: add and learn animation id if time permits
+                    overridePendingTransition(0, 0);
+                }
+                        return true;
             }
-            return true;
         });
 
 
     }
 
 
+
     public void goToLogin(){
         //TODO: erase user authentication
 
         //
-
-        Intent intent= new Intent(this, loginActivity.class);
-        startActivity(intent);
+        startActivity(new Intent(getApplicationContext(), loginActivity.class));
     }
+
+    @Override
+    public void onMapReady(@NonNull GoogleMap googleMap) {
+        mMap= googleMap;
+
+
+        fusedLocationClient = LocationServices.getFusedLocationProviderClient(this);
+
+
+
+        // Turn on the My Location layer and the related control on the map.
+        updateLocationUI();
+
+        // Get the current location of the device and set the position of the map.
+        getDeviceLocation();
+    }
+
+    private void getLocationPermission() {
+        /*
+         * Request location permission, so that we can get the location of the
+         * device. The result of the permission request is handled by a callback,
+         * onRequestPermissionsResult.
+         */
+        if (ContextCompat.checkSelfPermission(this.getApplicationContext(),
+                android.Manifest.permission.ACCESS_FINE_LOCATION)
+                == PackageManager.PERMISSION_GRANTED) {
+            locationPermissionGranted = true;
+        } else {
+            ActivityCompat.requestPermissions(this,
+                    new String[]{android.Manifest.permission.ACCESS_FINE_LOCATION},
+                    PERMISSIONS_REQUEST_ACCESS_FINE_LOCATION);
+        }
+    }
+
+
+
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode,
+                                           @NonNull String[] permissions,
+                                           @NonNull int[] grantResults) {
+        locationPermissionGranted = false;
+        if (requestCode
+                == PERMISSIONS_REQUEST_ACCESS_FINE_LOCATION) {// If request is cancelled, the result arrays are empty.
+            if (grantResults.length > 0
+                    && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                locationPermissionGranted = true;
+            }
+        } else {
+            super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        }
+        updateLocationUI();
+    }
+
+
+    private void updateLocationUI() {
+        if (mMap == null) {
+            return;
+        }
+        try {
+            if (locationPermissionGranted) {
+                mMap.setMyLocationEnabled(true);
+                mMap.getUiSettings().setMyLocationButtonEnabled(true);
+            } else {
+                mMap.setMyLocationEnabled(false);
+                mMap.getUiSettings().setMyLocationButtonEnabled(false);
+                lastKnownLocation = null;
+                getLocationPermission();
+            }
+        } catch (SecurityException e)  {
+            Log.e("Exception: %s", e.getMessage());
+        }
+    }
+
+
+    private void getDeviceLocation() {
+        /*
+         * Get the best and most recent location of the device, which may be null in rare
+         * cases when a location is not available.
+         */
+        try {
+            if (locationPermissionGranted) {
+                Task<Location> locationResult = fusedLocationClient.getLastLocation();
+                locationResult.addOnCompleteListener(this, new OnCompleteListener<Location>() {
+                    @Override
+                    public void onComplete(@NonNull Task<Location> task) {
+                        if (task.isSuccessful()) {
+                            // Set the map's camera position to the current location of the device.
+                            lastKnownLocation = task.getResult();
+                            if (lastKnownLocation != null) {
+                                mMap.addMarker(new MarkerOptions().position(new LatLng(lastKnownLocation.getLatitude(),
+                                                lastKnownLocation.getLongitude()))
+                                        .icon(bitmapDescriptorFromVector(getApplicationContext(), R.drawable.ic_user_location_marker)));
+
+                                mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(
+                                        new LatLng(lastKnownLocation.getLatitude(),
+                                                lastKnownLocation.getLongitude()), 15));
+                            }
+                        } else {
+                            Log.d(TAG, "Current location is null. Using defaults.");
+                            Log.e(TAG, "Exception: %s", task.getException());
+                            mMap.moveCamera(CameraUpdateFactory
+                                    .newLatLngZoom(uwMadisonLocation, 15));
+                            mMap.getUiSettings().setMyLocationButtonEnabled(false);
+                        }
+                    }
+                });
+            }
+        } catch (SecurityException e)  {
+            Log.e("Exception: %s", e.getMessage(), e);
+        }
+    }
+
+
+
+
+    //To make marker logo different
+    private BitmapDescriptor bitmapDescriptorFromVector(Context context, int vectorResId) {
+        Drawable vectorDrawable = ContextCompat.getDrawable(context, vectorResId);
+        vectorDrawable.setBounds(0, 0, vectorDrawable.getIntrinsicWidth(), vectorDrawable.getIntrinsicHeight());
+        Bitmap bitmap = Bitmap.createBitmap(vectorDrawable.getIntrinsicWidth(), vectorDrawable.getIntrinsicHeight(), Bitmap.Config.ARGB_8888);
+        Canvas canvas = new Canvas(bitmap);
+        vectorDrawable.draw(canvas);
+        return BitmapDescriptorFactory.fromBitmap(bitmap);
+    }
+
 }
